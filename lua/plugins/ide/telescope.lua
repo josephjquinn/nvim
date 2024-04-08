d3b67382c39ca92fb6f97a0ade730b591fa9e261
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-04-07 19:08:33 -0500

    adding telescope ui plugin

--- lua/plugins/ide/telescope.lua
+++ lua/plugins/ide/telescope.lua
@@ -13,7 +13,7 @@ local config = function()
 			find_files = {
 				previewer = true,
 				hidden = false,
-         file_ignore_patterns = { "node_modules" },
+				file_ignore_patterns = { "node_modules" },
 			},
 			live_grep = {
 				previewer = true,
@@ -30,9 +30,24 @@ local config = function()
 end
 
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.3",
-	lazy = false,
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = config,
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.3",
+		lazy = false,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = config,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
