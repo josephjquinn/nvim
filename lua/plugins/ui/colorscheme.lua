847631c24c8d5b0afe856320e69487dc9a93ceba
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-08-21 18:29:21 -0500

    updated colors

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -7,11 +7,21 @@ return {
 	},
 	{
 		"folke/tokyonight.nvim",
-		config = function()
-			vim.cmd([[colorscheme tokyonight]])
-		end,
 	},
 	{
 		"nyoom-engineering/oxocarbon.nvim",
+	},
+
+	{
+		"0xstepit/flow.nvim",
+
+	},
+
+	{
+		"sho-87/kanagawa-paper.nvim",
+
+		config = function()
+			vim.cmd([[colorscheme kanagawa-paper]])
+		end,
 	},
 }
